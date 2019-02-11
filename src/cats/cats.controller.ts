5042841bf05b63f2260ee32f72ae9a26a3d671df
bannzai <kingkong999yhirose@gmail.com>
2019-02-12 00:11:31 +0900

    :sparkles: Create cat controller and models

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+
+  @Post
+  create(@Body createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  async findAll(): Promise<any[]> {
+    return []
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    console.log(params.id);
+    return `This action returns a #${params.id}`;
+  }
+}
